e3eb84e46a405b7a5a4b284d902f2e86626e5eb1
austin <austinpham609@gmail.com>
2020-02-04 13:18:21 -0500

    absolutely nothing has changed so i don't know why netbeans wants me to commit

--- src/deliverable1/CardSuit.java
+++ src/deliverable1/CardSuit.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package deliverable1;
 
@@ -10,6 +8,7 @@ public enum CardSuit{
     HEARTS("Heart"), CLUBS("Clubs"), SPADES("Spades"), DIAMONDS("Diamonds");
     
     private final String suit;
+    
     CardSuit(String suit)
     {
         this.suit=suit;
