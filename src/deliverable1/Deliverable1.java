7e55d056639d224b9af96c9d0ba10997e5a0e335
austin <austinpham609@gmail.com>
2020-02-04 13:17:44 -0500

    shuffle cards and shit lol

--- src/deliverable1/Deliverable1.java
+++ src/deliverable1/Deliverable1.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package deliverable1;
 
@@ -14,19 +12,30 @@ public class Deliverable1 {
         
         Scanner input = new Scanner(System.in);
         
-        Deck player1Deck;
-        Deck player2Deck;
+        Deck player1Deck = new Deck(new LinkedList<Card>());;
+        Deck player2Deck = new Deck(new LinkedList<Card>());;
+        
+        //Generate all Cards
+        //And insert it into a
+        //List into the deck
+        LinkedList<Card> DeckList=new LinkedList<Card>();        
+        for(CardSuit s : CardSuit.values())
+        {
+            for (CardValue v : CardValue.values())
+            {
+                DeckList.addLast(new Card(s, v));
+            }
+        }
         
         //This deck contains all
         //Cards to be distributed
         //To both player's decks
-        Deck tempDeck;
+        Deck reserve = new Deck(DeckList);
         
         //Players are all to
         //Be stored into a linked list
         //for some future use
         LinkedList<Player> players = new LinkedList<Player>();
-        
         
         System.out.println("2 players are required for this game.");
         
@@ -38,8 +47,41 @@ public class Deliverable1 {
         System.out.println("Enter the second player's name:"); //Player 2 name
         players.addLast(new Player(input.next()));
         
-        System.out.println("The cards will be shuffled and distributed now.");
+        System.out.println("The cards will be shuffled and distributed now.");        
+        reserve.shuffleCards();
         
+        //
+        //Time to give one half
+        //To player 1 and the other
+        //Half to player two
+        
+        //To the first player
+        //I give you the first half of
+        //All the reserve cards
+        for (int i = 0; i < Math.ceil(reserve.getSize()/2); i++)
+        {
+            reserve.moveCard(i,player1Deck);
+        }
+        //System.out.println(player1Deck.toString()); //debug kek
+        
+        //Player two! This is 
+        //The last of my reserve cards.
+        //Now, take it from me!
+        //(Caeeeesar!)
+        for (int i = 0; i < reserve.getSize(); i++)
+        {
+            reserve.moveCard(i,player2Deck);
+        }
+        // System.out.println(player2Deck.toString()); //MY CODE SUCKS MORE ASS THAN A DONKEY FUCKER
+        
+        System.out.println("Finished distributing cards. The game will begin now.");
         
     }
 }
+
+
+
+
+
+//add me on discord telemachus#1793
+//kek
