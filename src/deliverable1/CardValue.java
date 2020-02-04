bf646e193cdfe167f4e87c8211a74acc11125851
austin <austinpham609@gmail.com>
2020-02-04 13:18:29 -0500

    absolutely nothing has changed so i don't know why netbeans wants me to commit

--- src/deliverable1/CardValue.java
+++ src/deliverable1/CardValue.java
@@ -1,23 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package deliverable1;
 
 public enum CardValue {
-    DEUCE(2), THREE(3), FOUR(4), 
+    DEUCE(2), THREE(3), FOUR(4),
     FIVE(5), SIX(6), SEVEN(7), EIGHT(8),
     NINE(9), TEN(10), JACK(11), QUEEN(12),
     KING(13), ACE(15);
-    
+
     private final int value;
-    
-    CardValue(int value)
-    {
-        this.value=value;
+
+    CardValue(int value) {
+        this.value = value;
     }
-    
+
     //returns value
     public int getValue()
     {
